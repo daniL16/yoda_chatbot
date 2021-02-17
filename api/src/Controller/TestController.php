617bafa00a5c43395df4c6904d408d69de3a60fa
dani <dani16lp@gmail.com>
2021-02-18 00:30:36 +0100

    first commit

---
+++ api/src/Controller/TestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use  App\Service\ChatBotApiService;
+class TestController
+{
+
+    public function index(){
+        $handler = new ChatBotApiService();
+        $response  = json_decode($handler->sendMessage('test'));
+        var_dump($response);
+        $response  = json_decode($handler->sendMessage('Que dise Juan',$response->sessionToken));
+        var_dump($response);
+        return new Response($response);
+    }
+}

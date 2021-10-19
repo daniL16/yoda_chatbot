adb7beb674d2a4ec67bdcc21881981659a51155f
daniL16 <daniL16@users.noreply.github.com>
2021-10-19 21:11:39 +0000

    Apply php-cs-fixer changes

--- api/src/Service/ChatBotApiService.php
+++ api/src/Service/ChatBotApiService.php
@@ -4,20 +4,16 @@ namespace App\Service;
 
 final class ChatBotApiService extends InbentaApiService
 {
-
     public function __construct()
     {
         parent::__construct();
         $this->baseUrl = $this->getApiUrl().'/'.$_ENV['INBENTA_API_VERSION'];
         $this->apiConfig = array_merge($this->apiConfig, [
             'new_conversation' => ['uri' => '/conversation', 'method' => 'POST', 'auth' => true],
-            'send_message' => ['uri' => '/conversation/message', 'method' => 'POST', 'auth' => true]
+            'send_message' => ['uri' => '/conversation/message', 'method' => 'POST', 'auth' => true],
         ]);
     }
 
-    /**
-     * @return string
-     */
     private function getApiUrl(): string
     {
         $response = json_decode($this->exec('get_apis')->getBody()->getContents());
@@ -47,7 +43,7 @@ final class ChatBotApiService extends InbentaApiService
             $conversation = $this->openConversation();
         }
 
-        $response = $this->exec('send_message', ['message' => $message], ['x-inbenta-session' => 'Bearer ' . $conversation]);
+        $response = $this->exec('send_message', ['message' => $message], ['x-inbenta-session' => 'Bearer '.$conversation]);
         $response = json_decode($response->getBody()->getContents());
 
         return ['session_token' => $conversation, 'response_message' => $response->answers[0]->message];
--- api/src/Service/InbentaSwapiApiService.php
+++ api/src/Service/InbentaSwapiApiService.php
@@ -4,7 +4,6 @@ namespace App\Service;
 
 class InbentaSwapiApiService extends InbentaApiService
 {
-
     private const N_FILMS = 5;
     private const N_PEOPLE = 5;
 
@@ -13,23 +12,17 @@ class InbentaSwapiApiService extends InbentaApiService
         parent::__construct();
         $this->baseUrl = $_ENV['INBENTA_SWAPI_URL'];
         $this->apiConfig = [
-            'api' => ['uri' => '/api', 'method' => 'POST', 'auth' => false]
+            'api' => ['uri' => '/api', 'method' => 'POST', 'auth' => false],
         ];
     }
 
-    /**
-     * @param string $query
-     * @param int $nItems
-     * @return string
-     */
-    private function makeQuery(string $query, int $nItems): string{
-        $response = $this->exec('api',  ['query' => $query, 'variables' => ['first' => $nItems]]);
+    private function makeQuery(string $query, int $nItems): string
+    {
+        $response = $this->exec('api', ['query' => $query, 'variables' => ['first' => $nItems]]);
+
         return $response->getBody()->getContents();
     }
 
-    /**
-     * @return string
-     */
     public function getFilms(): string
     {
         $query = <<<'GRAPHQL'
@@ -41,15 +34,13 @@ class InbentaSwapiApiService extends InbentaApiService
                         }
                     }
                   GRAPHQL;
-        $response = json_decode($this->makeQuery($query, self::N_FILMS),true);
-        return json_encode($response['data']['allFilms']['films']);
+        $response = json_decode($this->makeQuery($query, self::N_FILMS), true);
 
+        return json_encode($response['data']['allFilms']['films']);
     }
 
-    /**
-     * @return string
-     */
-    public function getPeople(): string{
+    public function getPeople(): string
+    {
         $query = <<<'GRAPHQL'
                  query allPeople($first:Int){
                        allPeople(first:$first){
@@ -59,7 +50,8 @@ class InbentaSwapiApiService extends InbentaApiService
                         }
                  }
                  GRAPHQL;
-        $response = json_decode($this->makeQuery($query, self::N_PEOPLE),true);
+        $response = json_decode($this->makeQuery($query, self::N_PEOPLE), true);
+
         return json_encode($response['data']['allPeople']['people']);
     }
 }
